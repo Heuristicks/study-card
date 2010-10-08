39bfe798440d24c6bd2f56adea9733cc0e28c93b
dpedroia15 <dpedroia15bosox@live.com>
2010-10-08 14:34:33 -0400

    Better implementation of loading text file. TODO: What's with the blank card after loading?; TODO: Saving; TODO: Graphics;

--- java/study-card/src/Stack.java
+++ java/study-card/src/Stack.java
@@ -25,26 +25,51 @@ public class Stack extends ArrayList<Card> {
     }
 
     public void Load(File f) throws FileNotFoundException, IOException {
-        ListIterator itr = this.listIterator();
-
         //Scanner fileScanner = new Scanner(f);
+        this.clear();
         FileReader fileReader = new FileReader(f);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
+        ArrayList<String> fileText = new ArrayList<String>();
+        String text;
+        while((text = bufferedReader.readLine()) != null) {
+            fileText.add(text);
+        }
+        bufferedReader.close();
+        fileReader.close();
+        ArrayList<String[]> cardsText = new ArrayList<String[]>();
+        for(int i = 0; i < fileText.size(); ++i) {
+            cardsText.add(ReturnCardCharacters(fileText.get(i),true));
+        }
+        SetSize(cardsText.size());
+        ListIterator itr = listIterator();
+        int placeholder = 0;
         while(itr.hasNext()) {
-            String text = bufferedReader.readLine();
-            if(!text.isEmpty()) {
-                String[] cardText = ReturnCardCharacters(text);
-                String front =  cardText[0];
-                String back = cardText[1];
-                ((Card)itr.next()).SetCharacters(front,back);
-            }
-            else {
-                break;
-            }
+             for(int i = 0; i < cardsText.get(placeholder).length; ++i) {
+                 String[] cardText = ReturnCardCharacters(cardsText.get(placeholder)[i],false);
+                 String front =  cardText[0];
+                 String back = cardText[1];
+                 ((Card)(itr.next())).SetCharacters(front,back);
+             }
+             ++placeholder;
         }
     }
 
-    private String[] ReturnCardCharacters(String charsToParse) {
-        return charsToParse.split("[,;]");
+    private String[] ReturnCardCharacters(String charsToParse, boolean wholeLine) {
+        if(wholeLine)
+            return charsToParse.split("[;]");
+        else
+            return charsToParse.split("[,]");
+    }
+
+    private void SetSize(int size) {
+        int thisSize = size();
+        if(size > thisSize) {
+            int cardsToAdd = size - thisSize;
+            for(int i = 0; i < cardsToAdd; ++i) {
+                add(new Card());
+            }
+            return;
+        }
+        return;
     }
 }
